123f6cadd94b38199a198ef9cb7092fb020007c5
Kevin Zuniga <77310855+K97IA@users.noreply.github.com>
2022-11-18 00:06:51 -0600

    Rename Ejemplo 21 - Matrices aleatorias.cpp to Ejemplo 25 - Matrices aleatorias.cpp

--- EJERCICIOS/Ejemplo 25 - Matrices aleatorias.cpp
+++ EJERCICIOS/Ejemplo 25 - Matrices aleatorias.cpp
@@ -1,6 +1,6 @@
 		
 #include <iostream>							//setlocale, cin, cout
-#include <stdlib.h>							//Null, memoria dinámica, control de procesos, rand(), srand()
+#include <stdlib.h>							//Null, memoria dinÃ¡mica, control de procesos, rand(), srand()
 #include <time.h>							//Funciones con fecha y hora
 #include <iomanip>							//setw
 
@@ -8,22 +8,22 @@ using namespace std;							//Nombre de espacio estandar en uso
 
 const int FIL{100};								//Constante para filas
 const int COL{100};								//Constante para columnas
-const int LI{-100};								//Límite inferior para números aleatorios
-const int LS{100};								//Límite superior para números aleatorios
+const int LI{-100};								//LÃ­mite inferior para nÃºmeros aleatorios
+const int LS{100};								//LÃ­mite superior para nÃºmeros aleatorios
 
-void llenarM(int [][COL], int, int);						//Prototipo función llenar matriz con números aleatorios note como colocamos el valor de la segunda dimensión.
-void imprimirM(int[][COL], int, int);						//Prototipo función Imprimir Matriz
-void imprimirM2(int[][COL], int, int);						//Prototipo función Imprimir Matriz con borde
-void sumarM(int[][COL], int[][COL], int[][COL], int, int);	//Prototipo función Sumar dos matrices
-int  mayores(int[][COL],int,int,int);						//Prototipo función contar números mayores a
+void llenarM(int [][COL], int, int);						//Prototipo funciÃ³n llenar matriz con nÃºmeros aleatorios note como colocamos el valor de la segunda dimensiÃ³n.
+void imprimirM(int[][COL], int, int);						//Prototipo funciÃ³n Imprimir Matriz
+void imprimirM2(int[][COL], int, int);						//Prototipo funciÃ³n Imprimir Matriz con borde
+void sumarM(int[][COL], int[][COL], int[][COL], int, int);	//Prototipo funciÃ³n Sumar dos matrices
+int  mayores(int[][COL],int,int,int);						//Prototipo funciÃ³n contar nÃºmeros mayores a
 
-int main(){													//Función principal
+int main(){													//FunciÃ³n principal
 	setlocale(LC_CTYPE,"Spanish");
 	int m1[FIL][COL]{0},m2[FIL][COL]{0}, m3[FIL][COL]{};									//Matrices a usar
-	int seguir{0}, f{0}, c{0}, n{0};														//Seguir, filas y columnas para la matriz además un número n como base y un contador.
+	int seguir{0}, f{0}, c{0}, n{0};														//Seguir, filas y columnas para la matriz ademÃ¡s un nÃºmero n como base y un contador.
 	system("cls");																			//Limpiar la pantalla
 	cout<<setw(15)<<"Bienvenid@ a este programa que suma dos matrices";
-	do{																						//Bloque para repetir mientras el usuario así lo desee
+	do{																						//Bloque para repetir mientras el usuario asÃ­ lo desee
 		cout<<endl;
 		cout<<setw(5)<<"Favor ingresar cantidad de filas de las matrices [1-100]: ";
 		do{																					//Bloque para validar filas entre 0 y 100
@@ -40,7 +40,7 @@ int main(){													//Función principal
 				cout<<setw(5)<<"Cantidad de columnas entre [1-100]: "; 
 			}	
 		}while((c<1)||(c>100));
-		//Ahora vamos a llenar las matric3s de fxc con números aleatorios
+		//Ahora vamos a llenar las matric3s de fxc con nÃºmeros aleatorios
 		llenarM(m1, f, c);
 		cout<<"\n Presione <<enter>> para continuar: ";
 		system("pause");
@@ -59,9 +59,9 @@ int main(){													//Función principal
 		imprimirM2(m3, f, c);
 		cout<<endl;
 		//Recorrer a la matriz con un criterio
-		cout<<"\n Ingrese un número para buscar sus mayores en M1+M2: ";
+		cout<<"\n Ingrese un nÃºmero para buscar sus mayores en M1+M2: ";
 		cin>>n;
-		cout<<"\n\n Esta es la cantidad de números mayores a "<<n<<" en M1+M2: "<<mayores(m3,f,c,n);
+		cout<<"\n\n Esta es la cantidad de nÃºmeros mayores a "<<n<<" en M1+M2: "<<mayores(m3,f,c,n);
 		cout<<"\n\nDesea seguir 1=si 2=no: ";
 		do{																					//Bloque para validar entrada para seguir
 			cin>>seguir;
@@ -74,42 +74,42 @@ int main(){													//Función principal
 }
 
 
-void llenarM(int x[][COL], int f, int c){						//Función llenar matriz con números aleatorios note como colocamos el valor de la segunda dimensión.
-	srand(time(NULL));											//Inicializar la función de números aleatorios usando TIME como semilla
-	for(int i=0; i<f; i++){										//Control de filas	  (dimensión 1)
-		for(int j{0}; j<c; j++){								//Control de columnas (dimensión 2)
-				x[i][j]	=	LI + rand()%(LS-LI);				//x[i][j] ahora será un número aleatorio entre los límites inferior y superior	
+void llenarM(int x[][COL], int f, int c){						//FunciÃ³n llenar matriz con nÃºmeros aleatorios note como colocamos el valor de la segunda dimensiÃ³n.
+	srand(time(NULL));											//Inicializar la funciÃ³n de nÃºmeros aleatorios usando TIME como semilla
+	for(int i=0; i<f; i++){										//Control de filas	  (dimensiÃ³n 1)
+		for(int j{0}; j<c; j++){								//Control de columnas (dimensiÃ³n 2)
+				x[i][j]	=	LI + rand()%(LS-LI);				//x[i][j] ahora serÃ¡ un nÃºmero aleatorio entre los lÃ­mites inferior y superior	
 		}		
 	}
 }
 
-void imprimirM(int x[][COL], int f, int c){						//Función Imprimir Matriz
+void imprimirM(int x[][COL], int f, int c){						//FunciÃ³n Imprimir Matriz
 	cout<<setw(12)<<" |";
 	for(int i{0};i<c;i++){
 		cout<<setw(5)<<i;
 	}
 	cout<<endl;
 	for(int i=0; i<f; i++){	
-		cout<<setw(10)<<i<<" |";								//Control de filas	  (dimensión 1)
-		for(int j{0}; j<c; j++){								//Control de columnas (dimensión 2)
-				cout<<setw(5)<<x[i][j];							//x[i][j] es un número aleatorio entre los límites inferior y superior	
+		cout<<setw(10)<<i<<" |";								//Control de filas	  (dimensiÃ³n 1)
+		for(int j{0}; j<c; j++){								//Control de columnas (dimensiÃ³n 2)
+				cout<<setw(5)<<x[i][j];							//x[i][j] es un nÃºmero aleatorio entre los lÃ­mites inferior y superior	
 		}	
 		cout<<endl;	
 	}
 }
 
-void sumarM(int x[][COL], int y[][COL], int z[][COL], int f, int c){	//Función Sumar dos matrices
-	for(int i=0; i<f; i++){												//Control de filas	  (dimensión 1)
-		for(int j{0}; j<c; j++){										//Control de columnas (dimensión 2)
-				z[i][j]	=	x[i][j]	+ y[i][j];							//M3[i][j] ahora será la suma de M1[i][j]	y M2[i][j]		
+void sumarM(int x[][COL], int y[][COL], int z[][COL], int f, int c){	//FunciÃ³n Sumar dos matrices
+	for(int i=0; i<f; i++){												//Control de filas	  (dimensiÃ³n 1)
+		for(int j{0}; j<c; j++){										//Control de columnas (dimensiÃ³n 2)
+				z[i][j]	=	x[i][j]	+ y[i][j];							//M3[i][j] ahora serÃ¡ la suma de M1[i][j]	y M2[i][j]		
 		}		
 	}
 }
 
-int  mayores(int x[][COL], int f,int c, int n){						//Función contar números mayores a
+int  mayores(int x[][COL], int f,int c, int n){						//FunciÃ³n contar nÃºmeros mayores a
 	int cuenta{0};
-	for(int i=0; i<f; i++){												//Control de filas	  (dimensión 1)
-		for(int j{0}; j<c; j++){										//Control de columnas (dimensión 2)
+	for(int i=0; i<f; i++){												//Control de filas	  (dimensiÃ³n 1)
+		for(int j{0}; j<c; j++){										//Control de columnas (dimensiÃ³n 2)
 				if(x[i][j]>n){
 					cuenta++;
 				}
@@ -118,7 +118,7 @@ int  mayores(int x[][COL], int f,int c, int n){						//Función contar números ma
 	return cuenta;
 }
 
-void imprimirM2(int x[][COL], int f, int c){							//Función Imprimir Matriz con borde
+void imprimirM2(int x[][COL], int f, int c){							//FunciÃ³n Imprimir Matriz con borde
 	cout<<setw(11)<<" |";
 	cout<<"*";
 	for(int i{0};i<c;i++){
@@ -127,7 +127,7 @@ void imprimirM2(int x[][COL], int f, int c){							//Función Imprimir Matriz con
 		cout<<"   ";
 	}
 	cout<<"*"<<endl;
-	for(int i=0; i<f; i++){										//Control de filas	  (dimensión 1)
+	for(int i=0; i<f; i++){										//Control de filas	  (dimensiÃ³n 1)
 		
 		cout<<setw(11)<<" |";	
 		cout<<"*";							
@@ -138,9 +138,9 @@ void imprimirM2(int x[][COL], int f, int c){							//Función Imprimir Matriz con
 	
 		cout<<setw(9)<<i<<" |";
 		cout<<"*";								
-		for(int j{0}; j<c; j++){								//Control de columnas (dimensión 2)
+		for(int j{0}; j<c; j++){								//Control de columnas (dimensiÃ³n 2)
 				cout<<"*";
-				cout<<setw(4)<<x[i][j];							//x[i][j] es un número aleatorio entre los límites inferior y superior	
+				cout<<setw(4)<<x[i][j];							//x[i][j] es un nÃºmero aleatorio entre los lÃ­mites inferior y superior	
 				cout<<" *";
 		}	
 		cout<<"*"<<endl;	
